073259d39a417b6a197221f04ed67a36ecc50621
AmirZalaghi <zalaghi.dev@gmail.com>
2025-08-11 23:02:59 +0330

    ADD: Pipeline and Validation

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,6 +1,11 @@
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import ProjectStatusEnum from '../enums/projectStatusEnum';
 
 export class CreateProjectDto {
+  @IsNotEmpty({ message: 'نام پروژه نمیتواند خالی باشد' })
+  @IsString({ message: 'نام پروژه باید یک رشته باشد' })
   name: string;
+
+  @IsEnum(ProjectStatusEnum, { message: 'وضعیت پروژه نامعتبر است.' })
   status: ProjectStatusEnum;
 }
